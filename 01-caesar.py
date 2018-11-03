354919340a29f565e334d683508c63463bf6c398
Sachin Lagamuwa <sachinlagamuwa@hotmail.com>
2018-11-03 21:49:29 +0530

    Added both Encryption and Decryption
    Added a method to get user choice for Encryption or Decryption and also get key from user

--- 01-caesar.py
+++ 01-caesar.py
@@ -1,4 +1,22 @@
-#Input
+def getMode():
+
+     while True:
+
+         print('Do you wish to encrypt or decrypt a message?')
+
+         mode = input().lower()
+
+         if mode in 'encrypt e decrypt d'.split():
+
+             return mode
+
+         else:
+
+             print('Enter either "encrypt" or "e" or "decrypt" or "d".')
+
+mode = getMode()
+
+#Input string
 inputtext = input ("Input text:")
 
 
@@ -7,8 +25,24 @@ alphabetset = "abcdefghijklmnopqrstuvwxyz"
 
 
 #Set Key
-key = 3 
+def getKey():
 
+     key = 0
+
+     while True:
+
+         print('Enter the key number (1-%s)' % (26))
+
+         key = int(input())
+
+         if (key >= 1 and key <= 26):
+
+             return key
+key = getKey() 
+
+
+if mode[0] == 'd':
+	key = -key
 
 #Define cipher variable
 cipher =''
@@ -22,4 +56,4 @@ for c in inputtext:
   cipher += alphabetset[(alphabetset.index(c)+key)%(len(alphabetset))]
 
 #Output
-print("The encrypted message is:" + cipher)
+print("The translated message is:" + cipher)
